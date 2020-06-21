15daa36f098d6724e4e1ba8373a8baa41a0fbf96
Mochammad Ihza Rizky Karim <56474969+ihzarizkyk@users.noreply.github.com>
2020-06-21 12:26:37 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php 
+
+//Syntax Pertama (Dasar)
+echo "<h1>DOKUMENTASI PEMBELJARAN PHP<h1>";
+
+// Membuat Variabel
+
+$var1 = "Variabel 1";
+$var2 = "Variabel 2";
+$var3 = 123;
+$var4 = "123 ini string";
+
+echo $var1;
+echo "<br>";
+echo $var2;
+echo "<br>";
+echo $var3;
+echo "<br>";
+echo $var4;
+
+//Concatenation (CONCAT - Penyambung Variabel dan String)
+
+echo $var1." ".$var2." ".$var3." ".$var4;
+
+ ?>
