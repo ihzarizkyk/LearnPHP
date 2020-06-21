b99aebc77a0fe3c4b11cfc1b406cdc8e73b160a3
Mochammad Ihza Rizky Karim <56474969+ihzarizkyk@users.noreply.github.com>
2020-06-21 17:01:12 +0700

    Add files via upload

---
+++ kondisi-if-else.php
@@ -0,0 +1,39 @@
+<?php 
+/*
+Author : Mochammad Ihza Rizky Karim
+*/
+
+// Pengkondisian If
+
+$a = 10;
+
+if($a == 10){
+	echo "Nilainya sama 10";
+}
+
+// Pengkondisian if else
+
+$b = 11;
+
+if($b==10){
+	echo "nilaimu sama dengan 11";
+}else{
+	echo "nilaimu nggak sama";
+}
+
+/*
+
+Output :
+
+Pengkondisian if :
+
+Nilainya sama 10
+
+Pengkondisian if else :
+
+nilaimu nggak sama
+
+*/
+
+
+ ?>
