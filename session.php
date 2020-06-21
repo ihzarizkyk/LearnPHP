b2d63ba305b0199b276d647938e5bdfe778f5d1b
Mochammad Ihza Rizky Karim <56474969+ihzarizkyk@users.noreply.github.com>
2020-06-21 17:04:09 +0700

    Add files via upload

---
+++ session.php
@@ -0,0 +1,12 @@
+<?php 
+
+// Untuk Memulai Sesi, Wajib Menuliskan session_start() diawal kode
+session_start();
+
+// Menghapus atau unset sesi
+session_unset();
+
+// Menghancurkan Sesi atau Menghapus permanen sesi
+session_destroy();
+
+ ?>
