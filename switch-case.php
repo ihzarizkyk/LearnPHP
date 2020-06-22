99b489a5bf12fa78bfe40e1e52930da2871a30c3
Mochammad Ihza Rizky Karim <56474969+ihzarizkyk@users.noreply.github.com>
2020-06-22 13:42:53 +0700

    Add files via upload

---
+++ switch-case.php
@@ -0,0 +1,48 @@
+<?php 
+/*
+Author : Mochammad Ihza Rizky Karim
+*/
+
+// Switch Case PHP
+
+$a = 100;
+
+switch($a){
+
+	case 10:
+	echo "nilaimu sangat rendah";
+	break;
+
+
+	case 10:
+	echo "nilaimu sangat rendah";
+	break;
+
+	case 40:
+	echo "nilaimu rendah";
+	break;
+
+	case 60:
+	echo "nilaimu cukup";
+	break;
+
+	case 80:
+	echo "nilaimu baik";
+	break;
+
+	case 100:
+	echo "nilaimu sangat Sempurna";
+	break;
+
+}
+
+
+/*
+Output :
+
+nilaimu sangat Sempurna
+
+
+*/
+
+ ?>
