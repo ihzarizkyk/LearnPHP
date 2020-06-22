bb682de01498cc81d7c7793201f60d14c8e021c5
Mochammad Ihza Rizky Karim <56474969+ihzarizkyk@users.noreply.github.com>
2020-06-22 17:58:56 +0700

    Add files via upload

---
+++ constant.php
@@ -0,0 +1,41 @@
+<?php 
+/*
+Author : Mochammad Ihza Rizky Karim
+*/
+
+// Konstanta (Constant) adalah nilai tetap selama program berjalan
+
+//CARA 1
+define("MAXSIZE",25);
+
+echo MAXSIZE;
+echo "<br>";
+
+//CARA 2
+echo constant("MAXSIZE");
+
+//CARA 3
+defined("PERCOBAAN");
+
+echo PERCOBAAN;
+
+/*
+Output :
+
+Hasil CARA 1 :
+
+25
+
+Hasil CARA 2 :
+
+25
+
+Hasil CARA 3 :
+
+undefined constant PERCOAAN, dimasa depan fungsi ini akan
+menghasilkan nilai error ketika PHP Versi Terbaru
+
+
+*/
+
+ ?>
