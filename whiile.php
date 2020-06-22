550e0d6dcf72c0c8145eae031c866653c610bd85
Mochammad Ihza Rizky Karim <56474969+ihzarizkyk@users.noreply.github.com>
2020-06-22 17:28:30 +0700

    Add files via upload

---
+++ whiile.php
@@ -0,0 +1,46 @@
+<?php 
+/*
+Author : Mochammad Ihza Rizky Karim
+*/
+
+// Perulangan While
+
+// Contoh 1
+$a = 1;
+
+while($a <= 10)
+{
+	echo $a++;
+}
+
+// Contoh 2
+$b = 1;
+
+while($b <= 10)
+{
+	echo $b;
+	echo $b++."<br>";
+}
+
+/*
+Output :
+
+Contoh 1 :
+
+1234567891011
+
+Contoh 2 :
+
+22
+33
+44
+55
+66
+77
+88
+99
+1010
+
+*/
+
+ ?>
