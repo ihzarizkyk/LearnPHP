9b52252675e791c5f339e0c27832c6b48f46882a
Mochammad Ihza Rizky Karim <56474969+ihzarizkyk@users.noreply.github.com>
2020-06-24 13:36:53 +0700

    Update function.php

--- function.php
+++ function.php
@@ -1,7 +1,46 @@
 <?php
 /*
 Author : Mochammad Ihza Rizky Karim
-
-COMING SOON....
 */
+
+
+// Membuat Fungsi
+
+function msg(){
+	echo "Halo, Selamat Datang";
+	echo "<br>";
+}
+
+// Memanggil Fungsi
+
+msg();
+
+// Membuat Fungsi dengan Parameter
+
+function kali($kali){
+	echo $kali**2;
+	echo "<br>";
+	return $kali;
+}
+
+// Memanggil Fungsi dan mengisinya dengan Argumen
+
+echo"<br>";
+echo "FUNGSI KALI"."<br>";
+kali(2);
+kali(4);
+kali(6);
+
+/*
+Output :
+
+Halo, Selamat Datang
+
+FUNGSI KALI
+4
+16
+36
+
+*/
+
 ?>
