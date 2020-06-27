5a4df4770cdc1e35fd167132df205a8ca7c507e7
Mochammad Ihza Rizky Karim <56474969+ihzarizkyk@users.noreply.github.com>
2020-06-27 14:56:36 +0700

    Update oop1.php

--- oop1.php
+++ oop1.php
@@ -19,6 +19,14 @@ class biodata{
 	function get_nama(){
 		return $this->nama;
 	}
+
+	function set_umur($umur){
+		$this->umur = $umur;
+	}
+
+	function get_umur(){
+		return $this->umur;
+	}
 } 
 
 // buat objek baru
@@ -26,16 +34,19 @@ $nama1 = new biodata;
 $nama2 = new biodata;
 $nama3 = new biodata;
 $umur1 = new biodata;
+$umur2 = new biodata;
 
 // set nama
 
 $nama3->set_nama("Abdul Muhsin");
+$umur2->set_umur(20);
 
 echo $nama1->nama = "Mochammad Ihza Rizky Karim"."<br>";
 echo $nama2->nama = "Ihza Rizky <br>";
 echo $umur1->umur = "Berumur ". 19;
 echo "<br>";
-echo $nama3->get_nama();
+echo $nama3->get_nama()."<br>";
+echo $umur2->get_umur();
 
 /*
 Output :
@@ -44,6 +55,7 @@ Mochammad Ihza Rizky Karim
 Ihza Rizky
 Berumur 19
 Abdul Muhsin
+20
 
 */
  ?>
